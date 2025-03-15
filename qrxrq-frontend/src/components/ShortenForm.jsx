0663ec8637b48e8116721ee5dd697ef2d620e7cc
Mikhail A <mikhail@landofcash.net>
2025-03-15 23:55:33 +0000

    added CNAME added loading spinner
    Signed-off-by: Mikhail A <mikhail@landofcash.net>

--- qrxrq-frontend/src/components/ShortenForm.jsx
+++ qrxrq-frontend/src/components/ShortenForm.jsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 const ShortenForm = ({ setShortUrl }) => {
     const [url, setUrl] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -15,17 +16,29 @@ const ShortenForm = ({ setShortUrl }) => {
             return;
         }
         setError("");
+        setLoading(true);
         try {
             const response = await api.post("/shorten", {originalUrl: url});
             setShortUrl(response.data.shortUrl);
         } catch (error) {
             setError(`Error shortening URL: ${error}`);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <div className="w-full">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <div className="w-full relative p-2">
+            {loading && (
+                <div className="absolute inset-0 bg-green-100/60 flex items-center justify-center rounded-md z-10">
+                    <svg className="animate-spin h-10 w-10 text-green-500" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 0116 0"></path>
+                    </svg>
+                </div>
+            )}
+
+            <form onSubmit={handleSubmit} className={`flex flex-col gap-2 ${loading ? "pointer-events-none" : ""}`}>
                 <Label htmlFor="urlInput" className="text-(--color-foreground)  font-medium">
                     Enter URL to Shorten
                 </Label>
